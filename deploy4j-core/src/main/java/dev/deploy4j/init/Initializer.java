2abc1ba5653b88c994b91495ecae71d09c6995fd
teggr <robin@tegg.me.uk>
2026-07-30 20:39:49 +0100

    test: refactor Initializer and InitializerTest for template resource handling

--- deploy4j-core/src/main/java/dev/deploy4j/init/Initializer.java
+++ deploy4j-core/src/main/java/dev/deploy4j/init/Initializer.java
@@ -4,6 +4,9 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Objects;
 
 public class Initializer {
 
@@ -13,40 +16,35 @@ public class Initializer {
    * Create config stub in config/deploy.yml and env stub in .deploy4j/secrets
    */
   public void init(boolean bundle) {
+    Path workingDirectory = Path.of(System.getProperty("user.dir"));
 
-    File deployFile = new File("config/deploy.yml");
+    File deployFile = workingDirectory.resolve("config/deploy.yml").toFile();
     if (deployFile.exists()) {
       log.info("Config file already exists in config/deploy.yml (remove first to create a new one)");
     } else {
       deployFile.getParentFile().mkdirs();
-      try {
-        FileUtils.copyInputStreamToFile(
-          getClass().getClassLoader().getResourceAsStream("templates/deploy.yml"),
-          deployFile
-        );
+      try (InputStream template = getTemplateStream("templates/deploy.yml")) {
+        FileUtils.copyInputStreamToFile(template, deployFile);
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
       log.info("Created configuration file in config/deploy.yml");
     }
 
-    File secretsFile = new File(".deploy4j/secrets");
+    File secretsFile = workingDirectory.resolve(".deploy4j/secrets").toFile();
     if(secretsFile.exists()) {
       log.info("Secrets file already exists in .deploy4j/secrets (remove first to create a new one)");
     } else {
       secretsFile.getParentFile().mkdirs();
-      try {
-        FileUtils.copyInputStreamToFile(
-          getClass().getClassLoader().getResourceAsStream("templates/secrets"),
-          secretsFile
-        );
+      try (InputStream template = getTemplateStream("templates/secrets")) {
+        FileUtils.copyInputStreamToFile(template, secretsFile);
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
       log.info("Created secrets file");
     }
 
-    File hooksFolder = new File(".deploy4j/hooks");
+    File hooksFolder = workingDirectory.resolve(".deploy4j/hooks").toFile();
     if(!hooksFolder.exists()) {
       hooksFolder.mkdirs();
       log.info("Created hooks folder");
@@ -54,6 +52,15 @@ public class Initializer {
 
     // TODO: bundle add maven dependency?
 
+  }
+
+  private InputStream getTemplateStream(String templatePath) {
+    ClassLoader classLoader = getClass().getClassLoader();
+    InputStream stream = classLoader.getResourceAsStream(templatePath);
+    if (stream == null) {
+      stream = getClass().getResourceAsStream("/" + templatePath);
+    }
+    return Objects.requireNonNull(stream, "Missing template resource: " + templatePath);
   }
 
 }
